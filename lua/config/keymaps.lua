88d6fa6fd763c5cb3f0730944bf7627f75c10fdc
Sebastian Pokładek <sebpok228@gmail.com>
2025-08-21 00:01:30 +0200

    small_changes

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,1 +1,16 @@
+local map = vim.keymap.set
+local opts = { noremap = true, silent = true }
+
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+map("i", "<C-c>", "<Esc>", opts)
+
+-- Better navigation in insert mode
+map("i", "<C-h>", "<Left>", opts)
+map("i", "<C-l>", "<Right>", opts)
+map("i", "<C-j>", "<Down>", opts)
+map("i", "<C-k>", "<Up>", opts)
+
+map("n", "<leader>w", ":w<CR>", opts) -- save
+map("n", "<leader>q", ":q<CR>", opts) -- quit
+map("n", "<leader>h", ":nohlsearch<CR>", opts) -- clear search highlight
